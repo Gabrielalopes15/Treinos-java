683d4b7e33617e1c52ffc57dfcf19cb64b768c40
Gabriela Lopes <gabylopes2000@gmail.com>
2025-04-16 08:59:34 -0300

    contrutores

---
+++ Contrutores/src/main/java/gabrielalopes/contrutores/Contrutores.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package gabrielalopes.contrutores;
+
+/**
+ *
+ * @author gabriela.lopes
+ */
+public class Contrutores {
+
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa ();
+    }
+}
---
+++ Contrutores/src/main/java/gabrielalopes/contrutores/Pessoa.java
@@ -0,0 +1,16 @@
+
+package gabrielalopes.contrutores;
+
+
+public class Pessoa {
+    String nome, cpf, email;
+    int telefone, id;
+    
+    
+    public Pessoa(String nome, String email, String cpf) {
+    this.nome = nome;
+    this.email = email;
+    this.cpf = cpf;
+}
+
+}
